e010c52f1f4f046405d68e740c249c437012a95b
Allmarr1 <mabelozerov@gmail.com>
2023-09-24 16:43:50 +0300

    created blast-dynamodb

--- terraform/main.tf
+++ terraform/main.tf
@@ -61,14 +61,39 @@ resource "aws_dynamodb_table" "blast-dynamodb" {
   name             = "blast-dynamodb"
   hash_key         = "ID"
   billing_mode     = "PAY_PER_REQUEST"
-  stream_enabled   = true
-  
+
   attribute {
     name = "ID"
     type = "S"
   }
 
-   lifecycle {
-    prevent_destroy = true
+  #  lifecycle {
+  #   prevent_destroy = true
+  # }
+}
+
+data "aws_iam_policy_document" "lambda_ro_accsess_to_dynamodb" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "dynamodb:GetItem",
+      "dynamodb:BatchGetItem",
+      "dynamodb:Scan",
+      "dynamodb:Query",
+      "dynamodb:ConditionCheckItem"
+    ]
+
+    resources = [aws_dynamodb_table.blast-dynamodb.arn]
   }
-}  
+}
+
+resource "aws_iam_policy" "lambda_ro_accsess_to_dynamodb" {
+  name = "lambda_ro_accsess_to_dynamodb"
+  policy = data.aws_iam_policy_document.lambda_ro_accsess_to_dynamodb.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_ro_accsess_to_dynamodb" {
+  role = "${var.name}-lambda"
+  policy_arn = aws_iam_policy.lambda_ro_accsess_to_dynamodb.arn
+}
