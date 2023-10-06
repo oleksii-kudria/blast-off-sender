373aeced1891591cfb83d58c9bc62cc29c612bcc
oleksii-kudria <aleksej.kudrja@gmail.com>
2023-10-06 11:25:23 +0300

    Updated handler for dynamodb

--- src/sender/handler.py
+++ src/sender/handler.py
@@ -2,10 +2,26 @@ import boto3
 import json
 import os
 
+client = boto3.client('dynamodb')
+
 def handler(event, context):
-    s3 = boto3.resource('s3')
-    bucket = os.environ['BUCKET']
-    key = 'sample.json'
-    obj = s3.Object(bucket, key)
-    body = obj.get()['Body'].read().decode('utf-8')
-    return json.loads(body)
+  table = os.environ['TABLE']
+  data = client.get_item(
+    TableName=table,
+    Key={
+        'id': {
+          'S': '005'
+        }
+    }
+  )
+
+  response = {
+      'statusCode': 200,
+      'body': json.dumps(data),
+      'headers': {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+  }
+
+  return response
